bbbc415d3bcd9e158f6c38afc4fd11666e4a0026
Fabiohenrique07 <fabiohenriquemorenodez@gmail.com>
2026-04-08 16:43:56 -0300

    iniciando tutorial de alunissagem

--- conjunto/conjunto.js
+++ conjunto/conjunto.js
@@ -2,9 +2,20 @@ function conjunto(){
     const letras = new Set();
     letras.add("a");
     letras.add("b");
-    letras.add("c");
 
-    console.log(letras.has("a"));
+    console.log(letras.has("g"));
+
+    //delete() é um método para remover um elemento do conjunto
+    letras.delete("c");
+    console.log(letras);
+
+    //Como no conjunto a chave é igual ao valor, os métodos values() e keys() retornam exatamente a mesma coleção.
+    for (const x of letras.values()){
+        console.log(x);
+    }
+    for (const x of letras.keys()){
+        console.log(x);
+    }
 
 }
 conjunto();
--- mapa/mapa.js
+++ mapa/mapa.js
@@ -2,11 +2,32 @@ function mapa(){
     const frutas = new Map();
 
     frutas.set("maçã", 500);
-    frutas.set("banana",300);
+    frutas.set("banana", 300);
     frutas.set("laranja", 200);
+    //frutas.set("pera", 200)
+    frutas.set("pera", 400)
 
-    let preco = frutas.get("banana");
-
+    let preco = frutas.get("pera");
     console.log(preco);
+
+    //size é uma propriedade que armazenao tamanho do mapa
+    console.log(frutas.size);
+
+    //O método has() retorna verdadeiro ou falso para um determinada chave
+    console.log(frutas.has('banana'));
+    frutas.forEach((valor, chave) => console.log(`${chave} = R$${valor}`))
+
+    //O método keys() retorno uma coleçõa com as chaves do mapa
+    //A estrutura de repetição for of intera sobre os valores de uma coleção 
+
+    for (const x of frutas.keys()){
+        console.log(x);
+    }
+
+    //values() é um método que retorna uma coleção contendo todos valores de um mapa
+    for (const x of frutas.keys()){
+        console.log(x);
+    }
+
 }
 mapa();
